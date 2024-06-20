2f6bad4a70fb3f027158aa23f0a50be5a8f13a31
Sandeep Poudel <snpoudelskt@gmail.com>
2024-06-20 12:57:10 -0400

    python script to preprocessing input is added

---
+++ preprocessing_input.py
@@ -0,0 +1,5 @@
+'''
+This script reads in input datasets and make them ready to be input into the hbv model
+Specifically, we need to have these variables (precip, tavg, latitude) ready
+for all the watersheds of interest for hbv rainfall runoff modeling 
+'''
